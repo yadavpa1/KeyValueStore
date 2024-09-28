4dbae2cf3ef909f8e40a1dee79bc5414f23072d0
Ashu Kumar <catchashu10@gmail.com>
2024-09-28 00:28:48 -0500

    Modified Client to contain funtion names in lowercase

--- keyvaluestore_client.cc
+++ keyvaluestore_client.cc
@@ -8,68 +8,76 @@
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using keyvaluestore::InitRequest;
-using keyvaluestore::InitResponse;
 using keyvaluestore::GetRequest;
 using keyvaluestore::GetResponse;
+using keyvaluestore::InitRequest;
+using keyvaluestore::InitResponse;
+using keyvaluestore::KeyValueStore;
 using keyvaluestore::PutRequest;
 using keyvaluestore::PutResponse;
 using keyvaluestore::ShutdownResponse;
-using keyvaluestore::KeyValueStore;
 
-class KeyValueStoreClient {
-    public:
+class KeyValueStoreClient
+{
+public:
     KeyValueStoreClient(std::shared_ptr<Channel> channel)
-    : stub_(KeyValueStore::NewStub(channel)) {}
-    
-    // Initialize the key-value store with a server name.
-    bool Init(const std::string& server_name) {
+        : stub_(KeyValueStore::NewStub(channel)) {}
+
+    // initialize the key-value store with a server name.
+    bool init(const std::string &server_name)
+    {
         InitRequest request;
         request.set_server_name(server_name);
 
         InitResponse response;
         ClientContext context;
 
-        Status status = stub_->Init(&context, request, &response);
-        if (!status.ok()) {
-            std::cerr << "Init RPC failed: " << status.error_message() << std::endl;
+        Status status = stub_->init(&context, request, &response);
+        if (!status.ok())
+        {
+            std::cerr << "init RPC failed: " << status.error_message() << std::endl;
             return false;
         }
         return response.success();
     }
 
-    // Shut down the key-value store.
-    bool Shutdown() {
+    // shut down the key-value store.
+    bool shutdown()
+    {
         google::protobuf::Empty empty;
         ShutdownResponse response;
         ClientContext context;
 
-        Status status = stub_->Shutdown(&context, empty, &response);
-        if (!status.ok()) {
-            std::cerr << "Shutdown RPC failed: " << status.error_message() << std::endl;
+        Status status = stub_->shutdown(&context, empty, &response);
+        if (!status.ok())
+        {
+            std::cerr << "shutdown RPC failed: " << status.error_message() << std::endl;
             return false;
         }
         return response.success();
     }
 
-    // Get a value associated with a key.
-    std::pair<std::string, bool> Get(const std::string& key) {
+    // get a value associated with a key.
+    std::pair<std::string, bool> get(const std::string &key)
+    {
         GetRequest request;
         request.set_key(key);
 
         GetResponse response;
         ClientContext context;
 
-        Status status = stub_->Get(&context, request, &response);
-        if (!status.ok()) {
-            std::cerr << "Get RPC failed: " << status.error_message() << std::endl;
+        Status status = stub_->get(&context, request, &response);
+        if (!status.ok())
+        {
+            std::cerr << "get RPC failed: " << status.error_message() << std::endl;
             return {"", false};
         }
         return {response.value(), response.key_found()};
     }
 
-    // Put a key-value pair into the store.
-    std::pair<std::string, bool> Put(const std::string& key, const std::string& value) {
+    // put a key-value pair into the store.
+    std::pair<std::string, bool> put(const std::string &key, const std::string &value)
+    {
         PutRequest request;
         request.set_key(key);
         request.set_value(value);
@@ -77,28 +85,28 @@ class KeyValueStoreClient {
         PutResponse response;
         ClientContext context;
 
-        Status status = stub_->Put(&context, request, &response);
-        if (!status.ok()) {
-            std::cerr << "Put RPC failed: " << status.error_message() << std::endl;
+        Status status = stub_->put(&context, request, &response);
+        if (!status.ok())
+        {
+            std::cerr << "put RPC failed: " << status.error_message() << std::endl;
             return {"", false};
         }
         return {response.old_value(), response.key_found()};
     }
 
-    private:
+private:
     // gRPC stub to access the service
     std::unique_ptr<KeyValueStore::Stub> stub_;
-    
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     KeyValueStoreClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
 
-    client.Init("TestServer");
-    client.Put("key1", "value1");
-    client.Get("key1");
-    client.Shutdown();
+    client.init("TestServer");
+    client.put("key1", "value1");
+    client.get("key1");
+    client.shutdown();
 
     return 0;
 }
-
