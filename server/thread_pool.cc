f48d72478ead4922732de0735c6ea76137433200
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2024-10-27 17:01:07 -0500

    Separated thread pool into its own file

---
+++ server/thread_pool.cc
@@ -0,0 +1,40 @@
+#include "thread_pool.h"
+
+ThreadPool::ThreadPool(size_t num_threads) : stop(false) {
+    for (size_t i = 0; i < num_threads; ++i) {
+        workers.emplace_back([this]() { this->worker_thread(); });
+    }
+}
+
+ThreadPool::~ThreadPool() {
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        stop = true;
+    }
+    condition.notify_all();
+    for (std::thread& worker : workers) {
+        worker.join();
+    }
+}
+
+void ThreadPool::enqueue(std::function<void()> task) {
+    {
+        std::lock_guard<std::mutex> lock(queue_mutex);
+        tasks.push(task);
+    }
+    condition.notify_one();
+}
+
+void ThreadPool::worker_thread() {
+    while (true) {
+        std::function<void()> task;
+        {
+            std::unique_lock<std::mutex> lock(this->queue_mutex);
+            this->condition.wait(lock, [this]() { return this->stop || !this->tasks.empty(); });
+            if (this->stop && this->tasks.empty()) return;
+            task = std::move(this->tasks.front());
+            this->tasks.pop();
+        }
+        task();
+    }
+}
