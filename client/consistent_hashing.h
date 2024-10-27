1022f9a59a47bc5602b6faa8036a914e5cb45237
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2024-10-27 15:43:36 -0500

    Added code for consistent hashing on client end

---
+++ client/consistent_hashing.h
@@ -0,0 +1,39 @@
+#ifndef CONSISTENT_HASHING_H
+#define CONSISTENT_HASHING_H
+
+#include <string>
+#include <vector>
+#include <map>
+#include <functional>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+
+class ConsistentHashing {
+public:
+    ConsistentHashing(
+        int num_replicas,
+        const std::vector<std::string>& partitions
+    );
+
+    std::string GetPartition(
+        const std::string& key
+    );
+
+    void AddPartition(
+        const std::string& partition
+    );
+
+    void RemovePartition(
+        const std::string& partition
+    );
+
+    void PrintHashRing();
+
+private:
+    int num_replicas;
+    std::vector<std::string> partitions;
+    std::map<unsigned long, std::string> hash_ring;
+};
+
+#endif // CONSISTENT_HASHING_H
