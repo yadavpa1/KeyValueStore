0de6cc9dccfab47529a4049527baa9831d744c05
Pratik-Sanghavi <sanghavipratikr@gmail.com>
2024-10-27 22:04:21 -0500

    Revert "Separated thread pool into its own file"
    This reverts commit f48d72478ead4922732de0735c6ea76137433200.

--- server/raft_server.h
+++ server/raft_server.h
@@ -11,8 +11,6 @@
 #include <condition_variable>
 #include <functional>
 
-#include "thread_pool.h"
-
 // GRPC stuff
 #include <grpc/grpc.h>
 #include <grpc++/server.h>
@@ -44,6 +42,26 @@ using keyvaluestore::ShutdownRequest;
 using keyvaluestore::ShutdownResponse;
 using keyvaluestore::DieRequest;
 using keyvaluestore::DieResponse;
+
+// Simple ThreadPool class
+class ThreadPool {
+public:
+    ThreadPool(size_t num_threads);
+    ~ThreadPool();
+
+    // Add a task to the queue
+    void enqueue(std::function<void()> task);
+
+private:
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+
+    std::mutex queue_mutex;
+    std::condition_variable condition;
+    bool stop;
+
+    void worker_thread();
+};
 
 enum class RaftState { 
     FOLLOWER,
